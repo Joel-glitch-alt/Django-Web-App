cb2ab69f420faf9ccbd1604d6e4f7293e76cfeb7
Addition <joeladdition@gmail.com>
2025-10-02 16:49:19 +0000

    updated

--- practical/settings.py
+++ practical/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'practical.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': [BASE_DIR / "templates", 'static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# MEDIA_URL = 'images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
